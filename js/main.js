8d1d4d5445674a6d92458b3481052127e94a1df9
maggie <meghandamen@gmail.com>
2024-08-17 15:31:40 -0400

    changed header into a video
    change the header and replaced with a video that auto plays for a animation

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-console.log("JavaScript file is linked");
-
-//changes the header img on breakpoints to the better sized photo
-function updateImage() {
-    const img = document.getElementById('responsive-img');
-    if (window.innerWidth >= 1200) {
-        img.src = '../images/quatroTittleCard-3.png';
-    } else if (window.innerWidth >= 768) {
-        img.src = '../images/quatroTittleCard-2.png';
-    } else {
-        img.src = '../images/quatroTittleCard.png';
-    }  
-}
-//page load (load on desktop, tablet and phone)
-updateImage();
-//window resize (makes it interactive )
-window.addEventListener('resize', updateImage);
 
 //---------------------------------------------------
 //changing text on home page
